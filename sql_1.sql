697927ad90a550a8dbe31496adba7480191cc2e1
petrzakharov <47098349+petrzakharov@users.noreply.github.com>
2019-01-28 20:59:53 +0300

    Update sql_1.sql

--- sql_1.sql
+++ sql_1.sql
@@ -4,39 +4,37 @@ films (
     id serial PRIMARY KEY,
     country VARCHAR (355),
     box_office INT,
-    year VARCHAR (355)
-    
+    year INT
 );
 
 
 INSERT INTO
     films
 VALUES
-    (‘The Shawshank Redemption’, 0001, ‘USA’, 59841469, ’1994’),
-    (‘The Green Mile ’, 0002, ‘USA’, 286801374, ’1999’),
-    (‘Forrest Gump’, 0003, ‘USA’, 677386686, ’1994’),
-    (‘Schindler's List’, 0004, ‘USA’, 321265768, ’1993’),
-    (‘Intouchables’, 0005, ‘France’, 426588510, ’2011’);
+    ('The Shawshank Redemption', 0001, 'USA', 59841469, 1994),
+    ('The Green Mile', 0002, 'USA', 286801374, 1999),
+    ('Forrest Gump', 0003, 'USA', 677386686, 1994),
+    ('Schindlers List', 0004, 'USA', 321265768, 1993),
+    ('Intouchables', 0005, 'France', 426588510, 2011);
+
 
 
 CREATE TABLE
 persons (
-    name_surname VARCHAR (355),
+name_surname VARCHAR (355),
     id serial PRIMARY KEY
 );
 
 
-
-
+);
 INSERT INTO
     persons
 VALUES
-(‘Тим Роббинс’, 1001),
-(‘Том Хэнкс’,2002),
-(‘Роберт Земекис’,3003),
-(‘Стивен Спилберг’,4004),
-(‘Омар Си’,5005);
-
+('Тим Роббинс', 1001),
+('Том Хэнкс',2002),
+('Роберт Земекис',3003),
+('Стивен Спилберг',4004),
+('Омар Си',5005);
 
 
 CREATE TABLE
@@ -49,15 +47,12 @@ persons2content (
     FOREIGN KEY (id_content)
     REFERENCES films (id)
 );
-
      
-
-
 INSERT INTO
     persons2content
 VALUES
-(1001, 0001, ‘actor’),
-(2002, 0002, ‘actor’),
-(3003, 0003, ‘producer’),
-(4004, 0004, ‘producer’),
-(5005, 0005, ‘actor’);
+(1001, 0001, 'actor'),
+(2002, 0002, 'actor'),
+(3003, 0003, 'producer'),
+(4004, 0004, 'producer'),
+(5005, 0005, 'actor');
